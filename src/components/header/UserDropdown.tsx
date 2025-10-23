e79b093e2d7299a5f70e61663829080c7902fe6a
has77san <hh77sn2001@gmail.com>
2025-10-23 21:20:15 +0300

    editsix

--- src/components/header/UserDropdown.tsx
+++ src/components/header/UserDropdown.tsx
@@ -1,7 +1,5 @@
 import { useState } from "react";
-import { DropdownItem } from "../ui/dropdown/DropdownItem";
 import { Dropdown } from "../ui/dropdown/Dropdown";
-import { Link } from "react-router";
 
 export default function UserDropdown() {
   const [isOpen, setIsOpen] = useState(false);
