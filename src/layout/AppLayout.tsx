397568b7afa2cac5308792699f563e4cad4fd54a
has77san <hh77sn2001@gmail.com>
2025-11-06 15:39:25 +0300

    ll

--- src/layout/AppLayout.tsx
+++ src/layout/AppLayout.tsx
@@ -4,26 +4,44 @@ import AppHeader from "./AppHeader";
 import Backdrop from "./Backdrop";
 import AppSidebar from "./AppSidebar";
 
+// استيراد الصورة
+import backgroundImage from "../../public/images/cards/WhatsApp Image 2025-11-05 at 13.45.19_85cdf7f8.jpg";
+
 const LayoutContent: React.FC = () => {
   const { isExpanded, isHovered, isMobileOpen } = useSidebar();
 
   return (
-    <div className="min-h-screen flex flex-row-reverse"> {/* ✅ نبدل الترتيب حتى السايدبار يصير يمين */}
-      {/* ✅ السايدبار */}
+    <div className="min-h-screen flex flex-row-reverse"> {/* السايدبار على اليمين */}
+      {/* السايدبار */}
       <div>
         <AppSidebar />
         <Backdrop />
       </div>
 
-      {/* ✅ المحتوى */}
+      {/* المحتوى */}
       <div
         className={`flex-1 transition-all duration-300 ease-in-out ${
           isExpanded || isHovered ? "lg:mr-[290px]" : "lg:mr-[90px]"
-        } ${isMobileOpen ? "mr-0" : ""}`}  // ✅ استبدلنا ml بـ mr
+        } ${isMobileOpen ? "mr-0" : ""}`}
       >
         <AppHeader />
-        <div className="p-4 mx-auto max-w-(--breakpoint-2xl) md:p-6">
-          <Outlet />
+
+        {/* خلفية الصورة + المحتوى */}
+        <div
+          className="relative p-4 mx-auto md:p-6 min-h-screen overflow-hidden max-w-[1536px]"
+          style={{
+            backgroundImage: `url(${backgroundImage})`,
+            backgroundSize: "cover",
+            backgroundPosition: "center",
+            backgroundRepeat: "no-repeat",
+          }}
+        >
+          {/* فلتر خفيف فوق الصورة (اختياري) */}
+
+          {/* المحتوى */}
+          <div className="relative z-10">
+            <Outlet />
+          </div>
         </div>
       </div>
     </div>
